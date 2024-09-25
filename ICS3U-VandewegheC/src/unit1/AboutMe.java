80ad7b87335a0bcbb22119dadfc2adc40a230b86
343417192 <343417192@C-MXL2393F2M.tdsb.on.ca>
2024-09-25 10:57:26 -0400

    AboutMe - Incomplete

---
+++ ICS3U-VandewegheC/src/unit1/AboutMe.java
@@ -0,0 +1,28 @@
+package unit1;
+/**
+ * Description:
+ * Date: Sept. 24, 2024
+ * @author Caden Vandeweghe
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		//Timetable
+		
+	System.out.println("Caden Vandeweghe \n Lawrence Park CI \n Grade 11 \n Ms. Kemp");
+	System.out.println("\n \"Go Panthers\"");
+	System.out.println("\n My Current Semester 1 Timetable");
+	System.out.println("\n____________________________________________________________________________________________________________________________________________________________________________________");
+	System.out.printf("\n\tDay:\t|\t\tPeriod 1:\t\t|\t\tPeriod 2:\t\t|\t\tPeriod 3:\t\t|\t\tPeriod 4:\t\t|");
+	System.out.println("\nMonday\t\t|Computer and Information Science\t|Healthy Active Living Education\t|English: Understanding Contemporary\t|Functions\t\t\t\t|");
+	System.out.println("Day 1\t\t|9:00 AM - 10:15 Am\t\t\t|10:20 AM - 11:40 AM\t\t\t|12:40 PM - 1:55 PM\t\t\t|2:00 PM - 3:15 PM\t\t\t|");
+	System.out.println("______________________________________________________________________________________________________________________________________________________________________________________");
+	System.out.println("\nTuesday\t\t|Healthy Active Living Education\t|Computer and Information Science\t|Functions\t\t\t\t|English: Understanding Contemporary\t|");
+	System.out.println("Day 2\t\t|9:00 AM - 10:15 Am\t\t\t|10:20 AM - 11:40 AM\t\t\t|12:40 PM - 1:55 PM\t\t\t|2:00 PM - 3:15 PM\t\t\t|");
+	System.out.println("______________________________________________________________________________________________________________________________________________________________________________________");
+	System.out.println("\nWednesday\t|Computer and Information Science\t|Healthy Active Living Education\t|English: Understanding Contemporary\t|Functions\t\t\t\t|");
+	System.out.println("Day 1\t\t|9:55 AM - 10:15 Am\t\t\t|10:20 AM - 11:40 AM\t\t\t|12:40 PM - 1:55 PM\t\t\t|2:00 PM - 3:15 PM\t\t\t|");
+
+	}
+
+}
