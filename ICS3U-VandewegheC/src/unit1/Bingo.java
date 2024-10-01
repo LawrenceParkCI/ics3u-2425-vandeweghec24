6cb69f6eaa9289a1867073a9e5c1e58330ac6859
343417192 <343417192@C-MXL2393F2M.tdsb.on.ca>
2024-10-01 10:12:29 -0400

    COmplete Bingo

---
+++ ICS3U-VandewegheC/src/unit1/Bingo.java
@@ -0,0 +1,46 @@
+package unit1;
+
+/**
+ * Description: This code prints out a Bingo card and the rules of the game
+ * Date: Sept. 26, 2024
+ * @author Caden Vandeweghe
+ */
+public class Bingo {
+	/**
+	 *
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+		// Print game introduction and rules
+		System.out.println("Bingo Card");
+
+		System.out.println("\t1. The caller randomly pulls a numbered bingo ball.");
+		System.out.println("\t2. The number is placed on the bingo board and called out.");
+		System.out.println("\t3. Players look for the called number on their bingo card.");
+		System.out.println("\t4. If the number is located, it is marked off.");
+		System.out.println("\t5. Steps 1 to 4 are repeated until a player matches the BINGO pattern.");
+		System.out.println("\t6. The winning player yells BINGO.");
+
+
+		// Print the Bingo card header
+		System.out.printf("\n%15s%15s%15s%15s%15s%n", "B", "I", "N", "G", "O");
+		System.out.println("-----------------------------------------------------------------------------------------");
+
+		// Print Bingo card rows
+		System.out.printf("%15s%15s%15s%15s%15s%n", "2", "20", "42", "60", "64");
+		System.out.printf("%15s%15s%15s%15s%15s%n", "14", "25", "32", "55", "70");
+		System.out.printf("%15s%15s%16s%14s%15s%n", "5", "18", "FREE", "53", "67");
+		System.out.printf("%15s%15s%15s%15s%15s%n", "12", "16", "31", "46", "75");
+		System.out.printf("%15s%15s%15s%15s%15s%n", "10", "22", "39", "59", "71");
+	}
+}
+
+
+
+
+
+
+
+
+
+
