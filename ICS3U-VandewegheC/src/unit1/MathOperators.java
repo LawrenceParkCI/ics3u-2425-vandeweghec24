49cbbece6af47ad90a5e2dbca3f4ae399e50d125
343417192 <343417192@C-MXL2393F2M.tdsb.on.ca>
2024-10-01 10:13:46 -0400

    Finishing on GIT hub

---
+++ ICS3U-VandewegheC/src/unit1/MathOperators.java
@@ -0,0 +1,77 @@
+package unit1;
+/**
+ * Description: 
+ * Date: Oct. 1, 2024
+ * @author 
+ */
+public class MathOperators {
+	/**
+	 *
+	 * @param args unused
+	 */
+	public static void main (String [] args) {
+		/*
+         Pre: Kind of Math, but not really
+         What do you notice is happening?
+		 */
+
+		//      System.out.println("Butter" + "fly");
+		// Prints out the word "Butterfly"
+		System.out.println("Butter" + "fly");
+
+		//      System.out.println("1 + 2 + 3 + 4 + 5");
+		// Prints out "1 + 2 + 3 + 4 + 5"
+		System.out.println("1 + 2 + 3 + 4 + 5");
+		//      System.out.println(1 + 2 + 3 + 4 + 5);
+		// Prints out the sum of "1 + 2 + 3 + 4 + 5"
+		System.out.println(1 + 2 + 3 + 4 + 5);
+		//      System.out.println(1 + 2 + "3 + 4 + 5");
+		// Prints out the sum of "1 + 2" then "3 + 4 + 5", creating a print of "33 + 4 + 5"
+		System.out.println(1 + 2 + "3 + 4 + 5");
+		//      System.out.println("1 + 2 + 3" + 4 + 5);
+		// Prints out "1 + 2 + 3" then 45, creating a print of 1 + 2 + 345
+		System.out.println("1 + 2 + 3" + 4 + 5);
+		//Why do you think the last two outputs act so differently?
+		// I think that them last two outputs act so differently because of how one part is in brackets and the other is not in brackets as before it was either the code was in brackets or not
+
+		//Summary: What are two possible roles of the + operator?
+		// It can show add numbers together or it can pull them together so they are side by side
+
+		/*
+         Part 1 
+         Figure out what is the meaning of each operator. Use print statements
+         to verify your answer and explain.
+		 */
+
+		// + means: Addition
+
+		// - means: Subtraction
+
+		// * means: Multiplication
+
+		// / means: Division
+
+
+
+		//Print the following expression: (3 + 2) * 5
+		System.out.println ("(3 + 2) * 5");
+		//Print the following expression: 3 + 2 * 5
+		System.out.println ("3 + 2 * 5");
+		//Was this expected? Why or why not? It was no expected as I thought the console would print the answer, not the equation
+		/*
+         Part 2 - Calculate Using Operators
+         Print out, using one println statement, what 45 degrees Celsius is in Fahrenheit
+
+         Print out, using a print and a println statement, what 900 Fahrenheit is in Celsius
+		 */     
+
+		/*
+         Part 3
+         Figure out what is the meaning of % as a math operator. Use print statements
+         to check your answer.
+		 */
+
+		//I think % means: 
+
+	}
+}
