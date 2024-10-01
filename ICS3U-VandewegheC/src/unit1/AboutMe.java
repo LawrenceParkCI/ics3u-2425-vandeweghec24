faca7fd7501f80fd1a3b29bdae26b3e2a1d46381
343417192 <343417192@C-MXL2393F2M.tdsb.on.ca>
2024-10-01 10:12:03 -0400

    This is the complete version for the AboutMe assignment

--- ICS3U-VandewegheC/src/unit1/AboutMe.java
+++ ICS3U-VandewegheC/src/unit1/AboutMe.java
@@ -1,28 +1,62 @@
 package unit1;
+
 /**
- * Description:
+ * Description: This code showcases some personal information, a slogan and my current semester 1 timetable for the week of Sept. 22, 2024
  * Date: Sept. 24, 2024
  * @author Caden Vandeweghe
  */
 public class AboutMe {
-
+	/**
+	 *
+	 * @param args unused
+	 */
 	public static void main(String[] args) {
-		//Timetable
-		
-	System.out.println("Caden Vandeweghe \n Lawrence Park CI \n Grade 11 \n Ms. Kemp");
-	System.out.println("\n \"Go Panthers\"");
-	System.out.println("\n My Current Semester 1 Timetable");
-	System.out.println("\n____________________________________________________________________________________________________________________________________________________________________________________");
-	System.out.printf("\n\tDay:\t|\t\tPeriod 1:\t\t|\t\tPeriod 2:\t\t|\t\tPeriod 3:\t\t|\t\tPeriod 4:\t\t|");
-	System.out.println("\nMonday\t\t|Computer and Information Science\t|Healthy Active Living Education\t|English: Understanding Contemporary\t|Functions\t\t\t\t|");
-	System.out.println("Day 1\t\t|9:00 AM - 10:15 Am\t\t\t|10:20 AM - 11:40 AM\t\t\t|12:40 PM - 1:55 PM\t\t\t|2:00 PM - 3:15 PM\t\t\t|");
-	System.out.println("______________________________________________________________________________________________________________________________________________________________________________________");
-	System.out.println("\nTuesday\t\t|Healthy Active Living Education\t|Computer and Information Science\t|Functions\t\t\t\t|English: Understanding Contemporary\t|");
-	System.out.println("Day 2\t\t|9:00 AM - 10:15 Am\t\t\t|10:20 AM - 11:40 AM\t\t\t|12:40 PM - 1:55 PM\t\t\t|2:00 PM - 3:15 PM\t\t\t|");
-	System.out.println("______________________________________________________________________________________________________________________________________________________________________________________");
-	System.out.println("\nWednesday\t|Computer and Information Science\t|Healthy Active Living Education\t|English: Understanding Contemporary\t|Functions\t\t\t\t|");
-	System.out.println("Day 1\t\t|9:55 AM - 10:15 Am\t\t\t|10:20 AM - 11:40 AM\t\t\t|12:40 PM - 1:55 PM\t\t\t|2:00 PM - 3:15 PM\t\t\t|");
+		// Personal information
+		System.out.println("Caden Vandeweghe \nLawrence Park CI \nGrade 11 \nMs. Kemp");
+		System.out.println("\n\"Go Panthers\"");
+		System.out.println("\nMy Current Semester 1 Timetable");
+		System.out.println("\n_______________________________________________________________________________________________________________________________________________________________________________");
+		// Timetable Header 
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", "Day", "Period 1", "Period 2", "Period 3", "Period 4");
+		System.out.println("\n_______________________________________________________________________________________________________________________________________________________________________________");
+		// Monday's schedule
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Monday", "Computer and Information Science", "Healthy Active Living Education", 
+				"English: Understanding Contemporary", "Functions");
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Day 1", "9:00 - 10:15", "10:20 - 11:40", "12:40 - 1:55", "2:00 - 3:15");
+		System.out.println("\n_______________________________________________________________________________________________________________________________________________________________________________");
+		// Tuesday's schedule
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Tuesday", "Healthy Active Living Education", "Computer and Information Science", 
+				"Functions", "English: Understanding Contemporary");
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Day 2", "9:00 - 10:15", "10:20 - 11:40", "12:40 - 1:55", "2:00 - 3:15");
+		System.out.println("\n_______________________________________________________________________________________________________________________________________________________________________________");
+		// Wednesday's schedule
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Wednesday", "Computer and Information Science", "Healthy Active Living Education", 
+				"English: Understanding Contemporary", "Functions");
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Day 1", "9:55 - 10:55", "11:00 - 12:00", "1:00 - 2:05", "2:10 - 3:15");
+		System.out.println("\n_______________________________________________________________________________________________________________________________________________________________________________");
+		// Thursday's schedule
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Thursday", "Healthy Active Living Education", "Computer and Information Science", 
+				"Functions", "English: Understanding Contemporary");
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Day 2", "9:00 - 10:15", "10:20 - 11:40", "12:40 - 1:55", "2:00 - 3:15");
+		System.out.println("\n_______________________________________________________________________________________________________________________________________________________________________________");
+		// Friday's schedule
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Friday", "Computer and Information Science", "Healthy Active Living Education", 
+				"English: Understanding Contemporary", "Functions");
+		System.out.format("\n%-10s | %-36s | %-36s | %-36s | %-36s", 
+				"Day 1", "9:00 - 10:15", "10:20 - 11:40", "12:40 - 1:55", "2:00 - 3:15");
+		System.out.println("\n_______________________________________________________________________________________________________________________________________________________________________________");
 
 	}
-
 }
+
+
+
