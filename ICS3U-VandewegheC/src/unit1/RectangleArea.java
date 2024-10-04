7191cf9fd01e692d171832543e1254e79e05827f
343417192 <343417192@C-MXL2393F2M.tdsb.on.ca>
2024-10-04 10:25:07 -0400

    Finished project for Rectangle Area

--- ICS3U-VandewegheC/src/unit1/RectangleArea.java
+++ ICS3U-VandewegheC/src/unit1/RectangleArea.java
@@ -1,5 +1,39 @@
 package unit1;
+import java.util.Scanner;
 
+/**
+ * Description: This code lets you type your first and last name as well as a subject
+ * Date: Oct. 3, 2024
+ * @author Caden Vandeweghe
+ */
 public class RectangleArea {
+	/**
+	 *
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		//declare variables
+		int length, width, depth;
+
+		//get the user input
+		System.out.println("VOLUME PROGRAM");
+		System.out.println("Type in the length of the rectange and <Enter>: ");
+		length = sc.nextInt();
+
+		System.out.println("Type in the width of the cube and <Enter>: ");
+		width = sc.nextInt();
+
+		System.out.println("Type in the depth of the cube and <Enter>: ");
+		depth = sc.nextInt();
+
+		// calculate the area
+		int volume = length * width * depth;
+
+		//print the output
+		System.out.println("The volume of your cube is" + volume);
+		sc.close();
+	}
 
 }
