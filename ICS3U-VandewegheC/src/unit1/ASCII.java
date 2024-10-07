2e0a06d961a32e6d4476b3e8de937f02856354e8
cadenvandeweghe <caden.vandeweghe2@student.tdsb.on.ca>
2024-10-07 00:13:42 -0400

    Almost completed ASCII.java

--- ICS3U-VandewegheC/src/unit1/ASCII.java
+++ ICS3U-VandewegheC/src/unit1/ASCII.java
@@ -1,5 +1,55 @@
 package unit1;
-
+/**
+ * Description: This program prints the author's name and a picture in ASCII style. 
+ * Date: Sept. 26, 2024
+ * @author Caden Vandeweghe
+ */
 public class ASCII {
+	/**
+	 *
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
 
+//This prints my name
+    System.out.println("____     _      ____  U _____ u _   _        __     __     _      _   _    ____  U _____ u            U _____ u   ____   _   _  U _____ u"); 
+ System.out.println("U /"___|U  /"\  u |  _"\ \| ___"|/| \ |"|       \ \   /"/uU  /"\  u | \ |"|  |  _"\ \| ___"|/__        __\| ___"|/U /"___|u|'| |'| \| ___"|/ "); 
+ System.out.println("\| | u   \/ _ \/ /| | | | |  _|" <|  \| |>       \ \ / //  \/ _ \/ <|  \| |>/| | | | |  _|"  \"\      /"/ |  _|"  \| |  _ /| |_| |\ |  _|"   "); 
+  System.out.println("| |/__  / ___ \ U| |_| |\| |___ U| |\  |u       /\ V /_,-./ ___ \ U| |\  |uU| |_| |\| |___  /\ \ /\ / /\ | |___   | |_| |U|  _  |u | |___   "); 
+  System.out.println(" \____|/_/   \_\ |____/ u|_____| |_| \_|       U  \_/-(_//_/   \_\ |_| \_|  |____/ u|_____|U  \ V  V /  U|_____|   \____| |_| |_|  |_____|  "); 
+  System.out.println("_// \\  \\    >>  |||_   <<   >> ||   \\,-.      //       \\    >> ||   \\,-.|||_   <<   >>.-,_\ /\ /_,-.<<   >>   _)(|_  //   \\  <<   >>  "); 
+System.out.println("(__)(__)(__)  (__)(__)_) (__) (__)(_")  (_/      (__)     (__)  (__)(_")  (_/(__)_) (__) (__)\_)-'  '-(_/(__) (__) (__)__)(_") ("_)(__) (__) "); 
+
+System.out.println("                                                     -:");                                     
+System.out.println("                                                     .:.:    ");                                  
+    System.out.println("                                              .::           ");                               
+    System.out.println("                                         ...::          ");                                   
+      System.out.println("                                      :**.                    ");                           
+       System.out.println("                                    =.-+#+*=.                    ");                       
+        System.out.println("                                   +=-==+*+*                        ");                   
+         System.out.println("                                 +#+*==+*+:                          ");                 
+               System.out.println("                         .+*++**.+**-                         ");                  
+          System.out.println("                               .= +##=###+.                        ");                  
+                 System.out.println("                          :-##*+*##+                        ");                  
+             System.out.println("                              :=*##*=*#=++*-:=-                 ");                  
+                    System.out.println("                        +***#*=*-=#*#+                   ");                  
+                       System.out.println("                     ++*#*#*=++#+##:                  ");                  
+                     System.out.println("                       =+*+#+++-+=*#+                   ");                  
+                System.out.println("                            #*=:=+#- =+=#                   ");                   
+               System.out.println("                            =+:   =*= : :=                    ");                  
+                System.out.println("                           *     +-+.  +.                    ");                  
+                System.out.println("                           +     =-+   =                      ");                 
+                   System.out.println("                        --.    +:  =.                      ");                 
+                   System.out.println("                          -   =.   :                       ");                 
+                    System.out.println("                            ..                            ");                 
+                      System.out.println("                                                        ");                 
+                        System.out.println("           -*+.=+: :+:.=-  -*=     :=..=:             ");                 
+                         System.out.println("          .#= :#+:#*  -#= :#=    -#+  =#-            ");                 
+                         System.out.println("          .#+:-: =#*  -#* :#=    +#+  =#*            ");                 
+                          System.out.println("         .#=    .#*  -#- :#=  .-:#+  =#-            ");                 
+                          System.out.println("         -+=.    .=:.-:  -+=::=: .=:.=.             ");                 
+                            System.out.println("                                                  ");                 
+                             System.out.println("        -. : : :.::  .  :.:.:: - ::              ");                 
+                               System.out.println("      .  . . . ..   .  ..  . .                 ");                 
+}
 }
