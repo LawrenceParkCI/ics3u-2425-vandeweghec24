125cb2503f9ccb6ff3286166431bd10b3c75496a
cadenvandeweghe <caden.vandeweghe2@student.tdsb.on.ca>
2024-10-06 20:05:00 -0400

    Finished PlayingWithStrings.java

--- ICS3U-VandewegheC/src/unit1/PlayingWithStrings.java
+++ ICS3U-VandewegheC/src/unit1/PlayingWithStrings.java
@@ -1,6 +1,6 @@
 package unit1;
 /**
- * Description:
+ * Description: This code outputs the uses of strings and what it can do.
  * Date: Oct. 4, 2024
  * @author Caden Vandeweghe
  */
@@ -48,8 +48,46 @@ public class PlayingWithStrings {
 		System.out.println("hello".substring(5));
 
 		//System.out.println("hello".substring(6));
+	
+		String spaces = "	Trim me		";
+		String noSpaces = spaces.trim();
+		System.out.println(spaces);
+		System.out.println(noSpaces);
 
+		String digits = "1234567890"
+		System.out.println(digits.indexOf("6") + digits.indexOf("a"));
+
+		System.out.println("Ilovesandwiches".substring(5).length());
+
+		String word1 = "Hello";
+		String word2 = word1;
+		word1 = word1.substring(1, 2);
+		System.out.printlnz(word2);
+
+		String name1 = "Pierre Elliott Trudeau";
+		String name2 = name1.substring(0, 6) + name 1.substring(15);
+		System.out.println(name2);
+
+		String msg = "I am enjoying this class.";
 		
+		String msg1 = msg.toUpperCase();
+		String msg2 = msg.toLowerCase();
+		char letter = msg.charAt(6);
+
+		System.out.println(msg);
+		System.out.println(msg1);
+		System.out.println(msg2);
+		System.out.println("Character at index 6 = " + letter);
+		System.out.println("msg has" + msg.length() + "characters.");
+
+		substring ( num 1, num2)
+		substring (num1)
+		charAt (num)
+		indexOf(letter)
+		length()
+		trim()
+		toUpperCase()
+		toLowerCase()
 	}
 
 }
