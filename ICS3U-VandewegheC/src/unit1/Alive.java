845897f0b81c0f1870e790a9f5d73c43bd0cd15f
cadenvandeweghe <caden.vandeweghe2@student.tdsb.on.ca>
2024-10-08 20:50:51 -0400

    Finished Alive.java
    Needs to be checked in class if it works

--- ICS3U-VandewegheC/src/unit1/Alive.java
+++ ICS3U-VandewegheC/src/unit1/Alive.java
@@ -1,7 +1,8 @@
 package unit1;
 
 import java.util.Scanner;
-
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 /**
  * Description: 
  * Date: Oct. 10, 2024
@@ -24,33 +25,50 @@ public class Alive {
 		int CurrentMonth;
 		int CurrentDay;
 
-				System.out.println("Type in your birth year and <Enter>: ");
-				BirthYear = sc.nextInt();
+		//Input for brith date questions
+		System.out.println("Enter your birthdate in numerical values:");
+				
+		System.out.println("\nType in your birth year and <Enter>: ");
+		BirthYear = sc.nextInt();
 
-				System.out.println("Type in your birth month and <Enter>: ");
-				BirthMonth = sc.nextInt();
+		System.out.println("\nType in your birth month and <Enter>: ");
+		BirthMonth = sc.nextInt();
 				
-				System.out.println("Type in your birth date and <Enter>: ");
-				BirthDate = sc.nextInt();
-				
-				System.out.println("Type in the current year and <Enter>: ");
-				CurrentYear = sc.nextInt();
+		System.out.println("\nType in your birth date and <Enter>: ");
+		BirthDate = sc.nextInt();
 
-				System.out.println("Type in the current month and <Enter>: ");
-				CurrentMonth = sc.nextInt();
-				
-				System.out.println("Type in the current day and <Enter>: ");
-				CurrentDay = sc.nextInt();
-				
-				System.out.println();
-								
-				int DaysAlive = ((CurrentYear - BirthYear)* 365) + ((CurrentMonth - BirthMonth)*30) + (CurrentDay - BirthDate);
+		//Input for current date questions
+		System.out.println("\nEnter the current date in numerical values:");
 
-				System.out.println("You have been alive for" );
-				
-				System.out.println("You have slept" + DaysAlive*8);
+		System.out.println("\nType in the current year and <Enter>: ");
+		CurrentYear = sc.nextInt();
 
+		System.out.println("\nType in the current month and <Enter>: ");
+		CurrentMonth = sc.nextInt();
 				
+		System.out.println("\nType in the current date and <Enter>: ");
+		CurrentDate = sc.nextInt();
 
+		//Acurate calculations using LocalDate
+		LocalDate BirthDay = LocalDate.of(BirthYear, BirthMonth, BirthDate);
+		LocalDate CurrentDaye= LocalDate.of(CurrentYear, CurrentMonth, CurrentDate);
+
+		//Exact Calculations
+		int daysAlive = ChronoUnit.DAYS.between(BirthDay, CurrentDay);
+		
+		//Approximate Calculations
+		int DaysAlive = ((CurrentYear - BirthYear)* 365) + ((CurrentMonth - BirthMonth)*30) + (CurrentDate - BirthDate);
+
+		int HourSlept = DaysAlive * 8;
+
+		//Output the result
+		System.out.println("\nYou have been alive for " + daysAlive + " days.");
+		
+		System.out.println("\nYou have been alive for " + DaysAlive + " days if there are 30 days in each month and 365 days per year.");
+			
+		System.out.println("\nYou have slept " + HoursSlept + " hours. if you sleep 8 hours a day");
+
+		sc.close();
+		
 	}
 }
