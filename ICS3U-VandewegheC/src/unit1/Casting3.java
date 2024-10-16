495cf7bd0cb2eed9345977b59a75ffd6324774ee
cadenvandeweghe <caden.vandeweghe2@student.tdsb.on.ca>
2024-10-16 00:24:35 -0400

    Update Casting3.java

--- ICS3U-VandewegheC/src/unit1/Casting3.java
+++ ICS3U-VandewegheC/src/unit1/Casting3.java
@@ -1,9 +1,9 @@
 package unit1;
 
 /**
-* Description:
-* Date:
-* @author 
+* Description: Casting 3 worksheet
+* Date: October 16, 2024
+* @author Caden Vandeweghe
 */
 public class Casting3 {
   
@@ -19,15 +19,17 @@ public class Casting3 {
     System.out.println("Part 1");
     System.out.println("1 + 2 + 3" + 4 + 5);
     //why did it print out this way?
-
+    // We did not specify that we wanted a mathematical operation to be outputted
     //Demonstrate casting a double value to a String in this way
     //Demonstrate casting a boolean value to a String in this way
     //Demonstrate casting a char value to a String in this way
-
+    System.out.println("1 + 2 + 3" + 4.5 + 5.5);
+    System.out.println("1 + 2 + 3" + true + false);
+    System.out.println("1 + 2 + 3" + 'a' + 'b');
 
     System.out.println("Part 2");
     //In order to change a String into an integer, we need another set of code
-
+    
     String strNum = "25";
     int myNum = Integer.parseInt(strNum);
 
@@ -41,12 +43,15 @@ public class Casting3 {
      * "-5"
     */
     //What can you say about how we can use the function, Integer.parseInt()?
+    // Only -5 works I think this is because Integer.parseInt() only works in converting integers, while everything other than -5 in the examples has a character that isn't an integer
+    double myNum2 = Double.parseDouble(strNum);
 
+    System.out.println(myNum2);
 
     // What do you think the code to change a String to double would look like?
     //Similarly, test out the code and write down what instructions
   
 
-    //When do you think it might be necessary to change a string value into an integer value/double value?
+    //When do you think it might be necessary to change a string value into an integer value/double value? If we are asking customers about our service through a rating.
   }
 }
