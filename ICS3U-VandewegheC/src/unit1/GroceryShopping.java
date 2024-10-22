029b384c0514327e8b40722c7be09e25c7ced062
343417192 <343417192@C-MXL2393F2M.tdsb.on.ca>
2024-10-22 11:40:28 -0400

    Finish at home

--- ICS3U-VandewegheC/src/unit1/GroceryShopping.java
+++ ICS3U-VandewegheC/src/unit1/GroceryShopping.java
@@ -1,6 +1,7 @@
 package unit1;
 
-import java.time.LocalDate;
+import java.text.DecimalFormat;
+
 import java.util.Scanner;
 
 /**
@@ -26,56 +27,63 @@ public class GroceryShopping {
 		double cost;
 		int amount;
 
+
+
+		DecimalFormat money = new DecimalFormat("$#,###,##0.00");
+
+
 		System.out.println("What would you like to buy? ");
-
-		buy = sc.nextLine(); 
-
+		buy = sc.nextLine();
 		System.out.println("How much does it cost? ");
-
 		cost = sc.nextDouble(); 
-
-
 		System.out.println("How many are you buying? ");
+		amount = sc.nextInt();
+		System.out.println("Very good.\n");
 
-		amount = sc.nextInt(); 
-
-		System.out.println("Very good.");
-
-		//Declare the variables
-		String buy2;
+		String buyTwo;
 		double cost2;
 		int amount2;
 
-		System.out.println("What would you like to buy? ");
+		sc.nextLine();
 
-		buy2 = sc.nextLine(); 
-
+		System.out.println("What else would you like to buy? ");
+		buyTwo = sc.nextLine();
 		System.out.println("How much does it cost? ");
-
-		cost2 = sc.nextDouble(); 
-
+		cost2 = sc.nextDouble();
 		System.out.println("How many are you buying? ");
-
 		amount2 = sc.nextInt(); 
-
 		System.out.println("Very good.");
-	
-		
-		
+
+		double totalCost = cost * amount;	
+		double totalCost2 = cost2 * amount2;	
+
+
 		System.out.println("\nThis is your receipt:");
-		
+
 		System.out.println("\n\n\t\t Mr. Vandeweghe's Grocery");
 
 		System.out.println("current date and time to do");
+
+		System.out.format("\n%-15s | %-15s | %-15s | %-20s", "Item", "Price", "Quant.", "Total Price");
+
+		System.out.println("\n-----------------------------------------------------------------");
+		System.out.format("%-15s | %-15s | %-15s | %-20s", buy, money.format(cost), amount, money.format(totalCost));
+		System.out.format("\n%-15s | %-15s | %-15s | %-20s", buyTwo, money.format(cost2), amount2, money.format(totalCost2));
+		System.out.println("\n-----------------------------------------------------------------");
 		
-		System.out.printf("\n%-15s | %-15s | %-15s | %-20s", "Item", "Price", "Quant.", "Total Price");
-		
-		System.out.println("\n--------------------------------------------------------------------------------");
-		
-		
-		
-		
-		
+		double subTotal = cost + cost2;
+		System.out.format("\n%45s", "Subtotal: ");
+		System.out.format("%-45s" + money.format(subTotal));
+		double taxAmount = 0.13;
+		double tax = subTotal * taxAmount;
+		System.out.format("\n%45s", "Tax: ");
+		System.out.format("%-45s"+ money.format(tax));
+
+
+
+
+
+
 		sc.close();
 	}
 }
