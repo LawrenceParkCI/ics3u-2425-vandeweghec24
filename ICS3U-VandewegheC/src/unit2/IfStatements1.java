c339c84770ccd8bde9ddd683b57b3127372c0602
cadenvandeweghe <caden.vandeweghe2@student.tdsb.on.ca>
2024-11-10 19:25:35 -0500

    Update IfStatements1.java

--- ICS3U-VandewegheC/src/unit2/IfStatements1.java
+++ ICS3U-VandewegheC/src/unit2/IfStatements1.java
@@ -1,8 +1,8 @@
 package unit2;
 /**
-* Description:
-* Date:
-* @author 
+* Description: IfStatements1 work sheet.
+* Date: Nov. 10, 2024
+* @author Caden Vandeweghe
 */
 
 public class IfStatements1 {
@@ -17,7 +17,7 @@ public class IfStatements1 {
       System.out.println("OMEGLUL");
     }    
 
-    //What prints out? Why?
+    //What prints out? Why? In part one, nothing is printed because 10 is not greater than 30
 
     System.out.println("\nPart 2: If, Else");
     System.out.println("----------------");
@@ -28,7 +28,7 @@ public class IfStatements1 {
       System.out.println("XQCL");
     }
 
-    //What prints out? Why?
+    //What prints out? Why? In part 2, "XQCL" is printed because the first condition is not satisfied therefore the else statement is illustrated.
 
     System.out.println("\nPart 3: If, Else if, Else");
     System.out.println("-------------------------");
@@ -41,7 +41,7 @@ public class IfStatements1 {
       System.out.println("XQCL");
     }
 
-    //What prints out? Why?
+    //What prints out? Why? In part 3, "sykSHY" is printed. This is because the first if statement is not satisfied and the next else if conditions are satisfied/ 
 
     System.out.println("\nPart 4: What's the difference?");
     System.out.println("------------------------------");
@@ -70,7 +70,18 @@ public class IfStatements1 {
     }
 
     //What prints out? Why?
-    //What is the difference between the first and second part?
+    //In part 4 this is printed out:
+	  //1
+	  //KEKW
+    //OMEGALUL
+    //2
+	  //KEKW
+    //OMEGALUL
+    //sykSHY
+	  //This is printed out because the first part the if statement conditions are met, and in the 
+    //second part the first and second if statements the conditions are satisfied
+	    
+    //What is the difference between the first and second part? The difference is the first part has an if, else if, and else statement. The second part has three if statement which allows more true conditions to be executed and printed
     
   }
 }
