8db109d9e52bd10012432983f6ce9c8a3f380485
cadenvandeweghe <caden.vandeweghe2@student.tdsb.on.ca>
2024-11-10 19:22:22 -0500

    Update IfChallenge2.java

--- ICS3U-VandewegheC/src/unit2/IfChallenge2.java
+++ ICS3U-VandewegheC/src/unit2/IfChallenge2.java
@@ -3,13 +3,34 @@ package unit2;
 import java.util.Scanner;
 
 /**
- * Description:
- * Date:
- * @author 
+ * Description: IfChallenge2 work sheet.
+ * Date: Nov. 10, 2024
+ * @author Caden Vandeweghe
 */
 
 public class IfChallenge2 {
   public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("Enter the first number: ");
+    int num1 = sc.nextInt();
+    
+    System.out.print("Enter the second number: ");
+    int num2 = sc.nextInt();
+    
+    System.out.print("Enter the third number: ");
+    int num3 = sc.nextInt();
+
+    // Check if the numbers are in strictly increasing order
+    if (num1 < num2 && num2 < num3) {
+      System.out.println("The numbers are in increasing order!");
+    } else {
+      System.out.println("The numbers are NOT in increasing order!");
+    }
+
+    scanner.close();
+    
     //This is for the portfolio
 
     /*Create a program that asks for three numbers.
