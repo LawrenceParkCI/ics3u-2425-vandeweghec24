77773c3461492f44417f31ccdf5c4e3615a7a389
343417192 <343417192@C-MXL2393F2M.tdsb.on.ca>
2024-11-19 10:16:16 -0500

    finish at home

--- ICS3U-VandewegheC/src/unit2/DoubleDiceContest.java
+++ ICS3U-VandewegheC/src/unit2/DoubleDiceContest.java
@@ -14,7 +14,47 @@ public class DoubleDiceContest {
 		 * then announce who won: the computer, the user, or say it was a tie.
 		 */
 
+		System.out.println("-------------------------------");
+
+		System.out.println("Your first number is:\n");
+		int user1 = (int)((Math.random() * 6) + 1);
+		System.out.println(user1);
+
+		System.out.println("\nYour second number is:\n");
+		int user2 = (int)((Math.random() * 6) + 1);
+		System.out.println(user2);
+
+		int totalUser = user1 + user2;
+
+		System.out.println("\nYou have a total of " + totalUser);
+
+		System.out.println("\n-------------------------------");
+
+		System.out.println("\nThe computers first number is:\n");
+		int computer1 = (int)((Math.random() * 6) + 1);
+		System.out.println(computer1);
+
+		System.out.println("\nThe computers second number is:\n");
+		int computer2 = (int)((Math.random() * 6) + 1);
+		System.out.println(computer2);
+
+		int totalComputer = computer1 + computer2;
+
+		System.out.println("\nThe computer has a total of " + totalComputer);
+
+		System.out.println("\n-------------------------------");
+
+		if (totalUser > totalComputer) {
+			System.out.println("\nYou win!");
+		} else if (totalUser == totalComputer) {
+			System.out.println("\nYou and the computer tied!");
+		} else { //(totalUser < totalComputer) {
+			System.out.println("\nThe computer wins!");
+		}
+		
+		System.out.println("\n-------------------------------");
 
 	}
-
 }
+
+
--- ICS3U-VandewegheC/src/unit2/Randomness.java
+++ ICS3U-VandewegheC/src/unit2/Randomness.java
@@ -6,22 +6,24 @@ https://www.geeksforgeeks.org/java-math-random-method-examples/
 
 Description:Randomness work sheet.
 Date: Nov. 18, 2024
-@author:
-*/
+@author: Caden Vandeweghe
+ */
 
 
 public class Randomness {
-  public static void main(String[] args) {
-    for (int i = 0; i < 10; i++) {
-      System.out.println(Math.random());
-    }
+	public static void main(String[] args) {
+		//for (int i = 0; i < 10; i++) {
+			//System.out.println(Math.random());
+		//}
 
-    //Run the program a few times. What do you think is the 
-    //range of the numbers generated?
-    
+		//Run the program a few times. What do you think is the 
+		//range of the numbers generated?
+		//0.01 to 0.99999999999999999
 
-    //Read the demo program. Write the code so that the program 
-    //will produce a random number between 100 and 1000.
-    
-  }
+		//Read the demo program. Write the code so that the program 
+		//will produce a random number between 100 and 1000.
+		for (int number = 0; number < 10; number++) {
+			System.out.println((Math.random() * 900) + 100);
+		} 
+	}
 }
