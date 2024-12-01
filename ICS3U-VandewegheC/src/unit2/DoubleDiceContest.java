ecfe9a0d884086f694b344971b43dcac90bdb533
cadenvandeweghe <caden.vandeweghe2@student.tdsb.on.ca>
2024-12-01 11:39:11 -0500

    Update DoubleDiceContest.java

--- ICS3U-VandewegheC/src/unit2/DoubleDiceContest.java
+++ ICS3U-VandewegheC/src/unit2/DoubleDiceContest.java
@@ -1,60 +1,58 @@
-package unit2;
 
+package unit2;
 /**
- * Description:
- * Date:
- * @author
+ * Description: This program does a rolling dice competition between the computer and the user
+ * Date: Dec. 1, 2024
+ * @author Caden Vandeweghe
  */
 public class DoubleDiceContest {
-
-	public static void main(String[] args) {
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
 		/*
 		 * Write code that simulates rolling two dice for the user and then two dice for the computer. 
 		 * The program should output the total of the two dice for both the user and the computer, 
 		 * then announce who won: the computer, the user, or say it was a tie.
 		 */
-
-		System.out.println("-------------------------------");
-
-		System.out.println("Your first number is:\n");
-		int user1 = (int)((Math.random() * 6) + 1);
-		System.out.println(user1);
-
-		System.out.println("\nYour second number is:\n");
-		int user2 = (int)((Math.random() * 6) + 1);
-		System.out.println(user2);
-
-		int totalUser = user1 + user2;
-
-		System.out.println("\nYou have a total of " + totalUser);
-
-		System.out.println("\n-------------------------------");
-
-		System.out.println("\nThe computers first number is:\n");
-		int computer1 = (int)((Math.random() * 6) + 1);
-		System.out.println(computer1);
-
-		System.out.println("\nThe computers second number is:\n");
-		int computer2 = (int)((Math.random() * 6) + 1);
-		System.out.println(computer2);
-
-		int totalComputer = computer1 + computer2;
-
-		System.out.println("\nThe computer has a total of " + totalComputer);
-
-		System.out.println("\n-------------------------------");
-
-		if (totalUser > totalComputer) {
-			System.out.println("\nYou win!");
-		} else if (totalUser == totalComputer) {
-			System.out.println("\nYou and the computer tied!");
-		} else { //(totalUser < totalComputer) {
-			System.out.println("\nThe computer wins!");
+		
+		//Introducion
+		System.out.println("Welcome to the dice competition!");
+		Thread.sleep(1000);
+		System.out.println("The user's dice are being rolled...");
+		Thread.sleep(1000);
+		//Roll user's dice
+		int die1 = (int)((Math.random()*6)+1);
+		int die2 = (int)((Math.random()*6)+1);
+		//Users score
+		int totalUDice = die1 + die2;
+		System.out.println("The user has a total dice score of: " + totalUDice);
+		Thread.sleep(1500);
+		//Computers score
+		System.out.println("\nThe computer's dice are being rolled...");
+		Thread.sleep(1500);
+		// roll comp's dice
+		int die3 = (int)((Math.random()*6)+1);
+		int die4 = (int)((Math.random()*6)+1);
+		// declare the computer's score
+		int totalCDice = die3 + die4;
+		System.out.println("The computer has a total dice score of: " + totalCDice);
+		
+		// declaring the winner
+		Thread.sleep(1500);
+		if (totalUDice > totalCDice) {
+			System.out.println("\nThe winner of the dice competition is the user!"); }
+		else if (totalUDice == totalCDice) {
+			System.out.println("\nThere is a tie!"); }
+		else {
+			System.out.println("\nThe winner of the dice competition is the computer!");
 		}
 		
-		System.out.println("\n-------------------------------");
+		
+		
 
 	}
+
 }
-
-
