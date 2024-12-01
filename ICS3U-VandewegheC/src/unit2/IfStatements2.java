316644903e6a2c044830439ac2d9f47d9e951ee1
cadenvandeweghe <caden.vandeweghe2@student.tdsb.on.ca>
2024-12-01 11:54:19 -0500

    Update IfStatements2.java

--- ICS3U-VandewegheC/src/unit2/IfStatements2.java
+++ ICS3U-VandewegheC/src/unit2/IfStatements2.java
@@ -1,33 +1,20 @@
 package unit2;
-
 import java.util.Scanner;
-
 /**
-* Description: IfStatements2 work sheet.
-* Date: Nov. 10, 2024
+* Description: This program teaches the programmer about if statements
+* Date: Nov. 5, 2024
 * @author Caden Vandeweghe
 */
-
 public class IfStatements2 {
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
   public static void main(String[] args) {
-
-    
-    Scanner in = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
     System.out.println("What is your age?");
-    int userAge = in.nextInt();
-
-     //Over 18
-    if (userAge > 18) { 
-        String title = "Adult";
-        System.out.println("title: "+ title);
-      } 
-    
-    //Not Over 18
-    else {  
-        System.out.println("Sorry, not quite yet.");
-      }
-    
+    int userAge = sc.nextInt();
     /*
     if (userAge > 18) { //If they're over 18, they are titled an adult
       String title = "Adult";
@@ -37,13 +24,20 @@ public class IfStatements2 {
 
     System.out.println(title);
     */
-    
     //Looking at the logic above, why do you think the 
-    //program won't work? This program looks to be all messed up and not in correct format.
+    //program won't work? 
 
+    // because an error will occur if the age is under 18 due to string title only happening if over 18.
+    
     //Copy the code below, and try to find a solution.
     //Comment the code above to avoid compilation errors.
 
-
+    if (userAge > 18) { //If they're over 18, they are titled an adult
+        String title = "Adult";
+        System.out.println(title);
+      } else {  //if they're not over 18, I don't want a title
+        System.out.println("Sorry, not quite yet.");
+      }
+    sc.close();
   }
 }
