bb94f64046017128caf9f8e9cd6c7f2f0a96483a
cadenvandeweghe <caden.vandeweghe2@student.tdsb.on.ca>
2025-01-22 14:55:58 -0500

    Update MethodChallenge1.java

--- ICS3U-VandewegheC/src/unit3/MethodChallenge1.java
+++ ICS3U-VandewegheC/src/unit3/MethodChallenge1.java
@@ -1,36 +1,38 @@
 package unit3;
-
 /**
-Description:
-Date:
-@author 
-*/
-
+Description: This is a method challenge
+Date: Dec 17, 2024
+@author Caden Vandeweghe
+ */
 public class MethodChallenge1 {
-   public static void main( String[] args )    {
-       // Complete the function below. Don't forget to comment.
-       double d1 = distance(-2,1 , 1,5);
-       System.out.println(" (-2,1) to (1,5) => " + d1 );
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
+	public static void main( String[] args )    {
+		// Complete the function below. Don't forget to comment.
+		double d1 = distance(-2,1 , 1,5);
+		System.out.println(" (-2,1) to (1,5) => " + d1 );
 
-       double d2 = distance(-2,-3 , -4,4);
-       System.out.println(" (-2,-3) to (-4,4) => " + d2 );
+		double d2 = distance(-2,-3 , -4,4);
+		System.out.println(" (-2,-3) to (-4,4) => " + d2 );
 
-       System.out.println(" (2,-3) to (-1,-2) => " + distance(2,-3,-1,-2) );
+		System.out.println(" (2,-3) to (-1,-2) => " + distance(2,-3,-1,-2) );
 
-       System.out.println(" (4,5) to (4,5) => " + distance(4,5,4,5) );
-   }
+		System.out.println(" (4,5) to (4,5) => " + distance(4,5,4,5) );
+	}
 
- /**
+	/**
  (Description)
- @param x1 ->
- @param y1 ->
+ @param x1 -> -2
+ @param y1 -> 1
  @param x2 ->
  @param y2 ->
  @return 
- */
-   public static double distance( int x1, int y1, int x2, int y2 )
-   {
-       // put your code up in here
-   return 0;
-   }
+	 */
+	public static double distance( int x1, int y1, int x2, int y2 )
+	{
+		double answer = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+		return answer;
+	}
 }
